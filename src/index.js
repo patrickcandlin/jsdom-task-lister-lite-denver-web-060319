203c5ab85e0d9e7cf12ff8dddc3721157ca36096
“Patrick <pctennislessons@gmail.com>
2019-07-20 22:44:03 -0600

    Done.

--- src/index.js
+++ src/index.js
@@ -1,36 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
-  
-  
-});
+  });
 
 const tasks = document.getElementById("tasks")
-const todoButton = document.getElementById("create-a-task").addEventListener('click', function(event){
-  event.preventDefault()
-})
-// const click = document.getElementByID("").addEventListener('click', deleteTask)
-
+const todoButton = document.getElementById("create-a-task")
+                            .addEventListener('click', event => event.preventDefault())
+//linked in the html 
 function handleClick(){
-  let todoContent = document.getElementById("new-task-description").value
-  todoButton
+  const todoContent = document.getElementById("new-task-description").value
   createTask(todoContent)
+  document.getElementById("create-task-form").reset()
 }
 
 function createTask(string) {
   const task = document.createElement('li')
-  task.id = 'task'
-  const deleteButton = document.createElement('button')
-  deleteButton.id = "delete-task"
-  deleteButton.innerText = "Delete"
-  deleteButton.addEventListener('click', deleteTask)
-  deleteButton.onclick = "deleteTask()"
   task.innerText = string
+  const deleteButton = document.createElement('button')
+  deleteButton.innerText = "Delete"
+  deleteButton.addEventListener('click', event => event.target.parentNode.remove())
   tasks.appendChild(task)
   task.appendChild(deleteButton)
 }
 
 function deleteTask() {
-  console.log('hello')
   const target = document.getElementById("task")
   tasks.removeChild(target)
-  
 }
